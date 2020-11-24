a40feeec5dce9744dca5af42198f2d4c6213bc4c
Ramon Lima <lima.ramon13@gmail.com>
2020-11-24 13:36:40 -0300

    Implementando lógica de verificação de outcome maior que valor total

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -15,6 +15,12 @@ class CreateTransactionService {
   }
 
   public execute({ title, value, type }: Request): Transaction {
+    const balance = this.transactionsRepository.getBalance();
+
+    if (type === 'outcome' && value > balance.total) {
+      throw Error("You don't have enough money for this transaction");
+    }
+
     const transaction = this.transactionsRepository.create({
       title,
       value,
